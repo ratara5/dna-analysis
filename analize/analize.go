a81b9bb7695f959280956bb6ef2e888f6e24e0b1
ratara5 <ratara5@gmail.com>
2023-09-28 08:27:42 -0500

    structure modification

--- analize/analize.go
+++ analize/analize.go
@@ -1,10 +1,7 @@
 package analize
 
 import (
-	"fmt"
 	"sync"
-	"time"
-	"github.com/ratara5/dna-analysis/util"
 )
 
 // CuatroEnFila comprueba la existencia de repeticiones de 4 en horizontal
@@ -74,58 +71,4 @@ func CuatroEnDiagonalAsc(matriz [][]string, canal chan int, wg *sync.WaitGroup, 
 		}
 	}
 	*goroutinesTerminadas++
-}
-
-//IsMutant Verifica si una secuencia de dna contenida en una matriz de nxn corresponde a la secuencia de un mutante
-func IsMutant(dna []string) bool {
-	//Generación de Matriz de DNA
-	resultadoMatriz := util.CrearMatriz(dna)
-	//-----------------------------------------------------------------------------------------------------
-	//TODO: Validar cadenas: que la longitud sea válida y que no haya caracteres diferentes de 'CAGT'
-	//TODO: Una secuencia de 5 caracteres repetidos cuenta como dos secuencias de 4 caracteres repetidos?
-	//-----------------------------------------------------------------------------------------------------
-
-	//Analisis de secuencia
-	///Canal para recibir conteo de cadenas de 4
-	resultadosCanal := make(chan int, 4)
-	///WaitGroup para esperar goroutines de revisión y conteo de cadenas de 4
-	var wg sync.WaitGroup
-	///Contador de rutinas terminadas
-	var goroutinesTerminadas int
-
-	///Llamado y adición a WaitGroup de goroutines de revisión y conteo de cadenas de 4
-	wg.Add(1)
-	go CuatroEnFila(resultadoMatriz, resultadosCanal, &wg, &goroutinesTerminadas)
-	wg.Add(1)
-	go CuatroEnColumna(resultadoMatriz, resultadosCanal, &wg, &goroutinesTerminadas)
-	wg.Add(1)
-	go CuatroEnDiagonalDesc(resultadoMatriz, resultadosCanal, &wg, &goroutinesTerminadas)
-	wg.Add(1)
-	go CuatroEnDiagonalAsc(resultadoMatriz, resultadosCanal, &wg, &goroutinesTerminadas)
-
-	///Comprobación continua de conteo de cadenas de 4
-	var sumaResultados int
-	for goroutinesTerminadas < 4 {
-		select {
-		case resultado := <-resultadosCanal:
-			sumaResultados += resultado
-			fmt.Println("Resultado:", resultado)
-			if sumaResultados == 2 {
-				//fmt.Println("La suma de resultados es igual a ", sumaResultados, ", el programa termina.")
-				return true
-			}
-		case <-time.After(time.Millisecond * 100):
-			// Comprobar en intervalos de tiempo si se cumple la condición
-			if sumaResultados == 2 {
-				//fmt.Println("La suma de resultados es igual a ", sumaResultados, ", el programa termina.")
-				return true
-			}
-		}
-	}
-
-	///Espera terminación de todas las goroutines
-	wg.Wait()
-	fmt.Println("Todas las GoRoutines han terminado!!!\nLa suma de resultados es igual a ", sumaResultados, ", el programa termina.")
-	return false
-
 }
